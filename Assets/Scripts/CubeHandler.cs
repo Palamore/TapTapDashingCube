dde43578e407a5def38d42ce1f1745bd8790e4b0
Gaminem9 <gaminem9@gmail.com>
2021-03-07 22:22:27 +0900

    [GameLogic] Cube 움직임 애니메이션 구현, 적용.
    Keyboard A and D will work

---
+++ Assets/Scripts/CubeHandler.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private static CubeHandler instance;
+
+    public static CubeHandler Instance()
+    {
+        if(instance == null)
+        {
+            instance = FindObjectOfType<CubeHandler>();
+        }
+        return instance;
+    }
+
+    private Animation cubeMoveAnimation;
+    private int invokeCnt;
+    public Transform RotationPoint;
+    private GameManager GM;
+    private float moveSpeed = 2.0f;
+    private bool inputFlag;
+
+    void Start()
+    {
+        invokeCnt = 0;
+        GM = GameManager.Instance();
+        inputFlag = true;
+    }
+
+    void Awake()
+    {
+        cubeMoveAnimation = this.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputFlag)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                RotationPoint = GM.LPoint.transform;
+                transform.SetParent(RotationPoint);
+                cubeMoveAnimation.Play("CubeMoveLeft");
+                MoveLeft();
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                RotationPoint = GM.RPoint.transform;
+                transform.SetParent(RotationPoint);
+                cubeMoveAnimation.Play("CubeMoveRight");
+                MoveRight();
+            }
+        }
+
+    }
+
+    public void MoveLeft()
+    {
+        inputFlag = false;
+        invokeCnt = 0;
+        InvokeRepeating("rotatingLeft", 0.0f, 1.0f / 320.0f);
+    }
+
+    public void MoveRight()
+    {
+        inputFlag = false;
+        invokeCnt = 0;
+        InvokeRepeating("rotatingRight", 0.0f, 1.0f / 320.0f);
+    }
+
+    private void rotatingLeft()
+    {
+        RotationPoint.rotation = Quaternion.Euler(moveSpeed * invokeCnt, -45.0f, 0.0f);
+        invokeCnt++;
+
+        if (invokeCnt >= 180 / moveSpeed)
+        {
+            CancelInvoke("rotatingLeft");
+            transform.parent = null;
+            transform.rotation = Quaternion.Euler(0.0f, 45.0f, 0.0f);
+            invokeCnt = 0;
+            GM.DestroyPoints();
+            GM.MakePoints();
+            inputFlag = true;
+        }
+    }
+
+    private void rotatingRight()
+    {
+        RotationPoint.rotation = Quaternion.Euler(moveSpeed * invokeCnt, 45.0f, 0.0f);
+        invokeCnt++;
+
+        if (invokeCnt >= 180 / moveSpeed)
+        {
+            CancelInvoke("rotatingRight");
+            transform.parent = null;
+            transform.rotation = Quaternion.Euler(0.0f, 45.0f, 0.0f);
+            invokeCnt = 0;
+            GM.DestroyPoints();
+            GM.MakePoints();
+            inputFlag = true;
+        }
+    }
+
+
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    static GameManager instance;
+
+    public static GameManager Instance()
+    {
+        if(instance == null)
+        {
+            instance = FindObjectOfType<GameManager>();
+        }
+        return instance;
+    }
+
+    public GameObject LPointPrefab;
+    public GameObject RPointPrefab;
+    private GameObject cube;
+
+    public GameObject LPoint;
+    public GameObject RPoint;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cube = CubeHandler.Instance().gameObject;
+        MakePoints();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void MakePoints()
+    {
+        LPoint = Instantiate(LPointPrefab, cube.transform.position + new Vector3(-0.5f, 0.0f, 0.5f), Quaternion.Euler(0.0f, -45.0f, 0.0f));
+        RPoint = Instantiate(RPointPrefab, cube.transform.position + new Vector3( 0.5f, 0.0f, 0.5f), Quaternion.Euler(0.0f,  45.0f, 0.0f));
+    }
+
+    public void DestroyPoints()
+    {
+        Destroy(LPoint);
+        Destroy(RPoint);
+    }
+
+
+}
