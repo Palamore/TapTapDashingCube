d3c81e99207dcfd2b89064e68bf1dab404ed45e0
jangheesun <swj04482@naver.com>
2021-05-21 19:32:05 +0900

    add0521

---
+++ Assets/textblink.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class textblink : MonoBehaviour
+{
+    Text flashingText; // Use this for initialization
+    
+    void Start () { 
+        flashingText = GetComponent<Text> (); 
+            StartCoroutine (BlinkText()); 
+    } 
+    
+    public IEnumerator BlinkText(){
+        while (true) { 
+            flashingText.text = ""; 
+            yield return new WaitForSeconds (.1f); 
+            flashingText.text = "Tap to Start"; 
+            yield return new WaitForSeconds (.3f); 
+        } 
+    } 
+}
